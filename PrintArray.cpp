2d896ea94deaf46257949d88410a23b06cd327f0
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-02-21 22:40:57 +0530

    Create PrintArray.cpp

---
+++ PrintArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int array[], int arrayLength, bool new_line=true){
+    for (int i=0; i<arrayLength; i++){
+        if (!new_line){
+            cout << array[i] << " ";
+        }
+        else{
+        cout << array[i] << endl;
+        }
+    }
+    if (!new_line){
+        cout << endl;
+    }
+}
+
+/*
+int main(){
+    int a[5] = {1, 2, 3, 4, 5}, n = 5;
+    // printArray({1, 2, 3, 4}, 4);
+    printArray(a, n);
+    return 0;
+}
+*/
