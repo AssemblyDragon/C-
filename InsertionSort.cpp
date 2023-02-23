22b6c9483ea86b8a17f7d7f231a4b2388cfbe6a8
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-02-23 21:40:44 +0530

    Create InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "PrintArray.cpp"
+using namespace std;
+
+void insertionSort(int a[], int n){
+    int buffer, index;
+    for (int i=1; i<n; i++){
+        buffer = a[i];
+        index = i-1;
+        while (index >= 0 && a[index] > buffer){
+            a[index+1] = a[index];
+            a[index] = buffer;
+            index--;
+        }      
+    }
+    printArray(a, n);
+}
+
+int main(){
+    int a[6] = {6, 1, 3, 9, 2, 6}, n = 6;
+    insertionSort(a, n);
+    return 0;
+}
