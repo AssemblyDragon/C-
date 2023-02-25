280ca570e5064fa55fa341314f9c319c50ef94ef
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-02-25 23:16:40 +0530

    Create RecordBreakingDayKickStart.cpp

---
+++ RecordBreakingDayKickStart.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=8, a[8] = {1, 2, 0, 7, 2, 0, 2, 2}, max;
+    max = a[0];
+    for (int i=1; i<n; i++){
+        if (a[i] > a[i-1] && i<n-1 && a[i] > a[i+1]){
+            max = a[i];
+        }
+        else if(a[i] > a[i-1] && a[i] > a[i+1]){
+            max = a[i];
+        }
+    }
+    cout << max;
+}
