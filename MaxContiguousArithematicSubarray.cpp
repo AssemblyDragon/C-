5114e769f5eae96ee6c56f7de422f6039d7cf4b9
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-02-25 23:17:52 +0530

    Create MaxContiguousArithematicSubarray.cpp

---
+++ MaxContiguousArithematicSubarray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=10, a[10] = {1, 2, 3, 4, 5, 4, 6, 8, 10, 1}, max, s, diff, oldiff;
+    oldiff = a[1] - a[0];
+    s = 2;
+    max = s;
+    for (int i=2; i<n; i++){
+        diff = a[i] - a[i-1];
+        if (oldiff == diff){
+            s++;
+        }
+        else if (s > max){
+            max = s;
+            oldiff = a[i] - a[i-1];
+            s  = 2;
+        }
+        else{
+            oldiff = a[i] - a[i-1];
+            s  = 2;
+        }
+    }
+    if (s>max){
+        max = s;
+    }
+    cout << max;
+    return 0;
+}
