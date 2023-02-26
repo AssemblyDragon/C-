24cab5a99b3793431c5cf7ee906c5e31f92147b9
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-02-26 23:38:16 +0530

    Create SubarrayWithGivenSum.cpp

---
+++ SubarrayWithGivenSum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    const int N=7;
+    int a[N] = {1, 2, 3, 1, 5}, s = 12;
+    int prev = 0, sum = 0;
+    bool k = false;
+    for (int i=0; i<N; i++){
+        sum += a[i];
+        if (sum > s){
+            sum -= a[prev];
+            prev ++;
+        }
+        if(sum == s){
+            cout << "Starting Index : " << prev << "\nEnding Index : " << i << endl;
+            k = true;
+            break;
+        }
+    }
+    if (!k){
+        cout << -1;
+    }
+
+}
