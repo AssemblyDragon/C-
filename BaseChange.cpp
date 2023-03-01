03ab1ffd4115e9dfab0b0210413d4aece375e105
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-03-01 23:25:16 +0530

    Create BaseChange.cpp

---
+++ BaseChange.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int pow(int n1, int n2){
+    if (n2 == 0){
+        return 1;
+    }
+    int num = n1;
+    for (int i=1; i<n2; i++){
+        num *= n1;
+    }
+    return num;
+}
+
+void convert(int num, int inputBase, int targetBase){
+    int factor, mul, n=0;
+
+    if (inputBase != 10){
+        int fact, temp, digit, n2=0;
+        for (int i=0;;i++){
+            if (pow(10, i) > num){
+                break;
+            }
+        
+            fact = i;
+        }
+
+        for (int i=0; i<=fact; i++){
+            n += num%10 * pow(inputBase, i);
+            num /= 10;
+        }
+
+    num = n;
+
+    }
+
+    if (targetBase == 10){
+        cout << endl << "Num = " <<  num << endl;
+        return;
+    }
+
+    for (int i=0;;i++){
+        if (pow(targetBase, i) > num){
+            break;
+        }
+        
+        factor = i;
+    }
+    cout << endl << "Num = ";
+    for (int i=factor; i>=0; i--){
+        for (int j=0;; j++){
+            if (pow(targetBase, i)*j > num){
+                break;
+            }
+            mul = j;
+        }
+        switch(mul){
+            case 10:
+                cout << "A";
+                break;
+            case 11:
+                cout << "B";
+                break;
+            case 12:
+                cout << "C";
+                break;
+            case 13:
+                cout << "D";
+                break;
+            case 14:
+                cout << "E";
+                break;
+            case 15:
+                cout << "F";
+                break;
+            default:
+                cout << mul;
+                break;
+        }
+        num -= mul*pow(targetBase, i);
+    }
+    
+    cout << endl;
+
+}
+
+int main(){
+    convert(101101, 2, 16);
+    
+    
+    /*
+    int n;
+    cout << "Enter No of elements in array : ";
+    cin >> n;
+    int array[n];
+    for (int i=0; i<n; i++){
+        cout << "Enter Element : ";
+        cin >> array[i];
+    }
+    for (int i=0; i<n; i++){
+        cout << i << " " << array[i] << endl;
+    }
+
+    int el;
+    cout << "Enter element to find : ";
+    cin >> el;
+    */
+    
+    return 0;
+
+}
