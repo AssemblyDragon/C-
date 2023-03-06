172e21a9b915221c12fc8ab81c34a1c097f617a0
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-03-06 23:49:41 +0530

    Update MaxSubarraySum.cpp

--- MaxSubarraySum.cpp
+++ MaxSubarraySum.cpp
@@ -3,10 +3,10 @@
 #include "PrintArray.cpp"
 using namespace std;
 
-int bruteForce(int a[], const int N){
-    int sum, max = INT_MIN;
+void bruteForce(int a[], const int N){
+    int sum = 0, max = INT_MIN;
     for (int i=0; i<N; i++){
-        for (int j=i+1; j<N; j++){
+        for (int j=i+1; j<N+1; j++){
             for (int k=i; k<j; k++){
                 sum += a[k];
             }
@@ -16,12 +16,39 @@ int bruteForce(int a[], const int N){
             sum = 0;
         }
     }
-    if ()
+    if (max == INT_MIN){
+        cout << "How is this even possible!" << endl;
+    }
+    else{
+        cout << max << endl;
+    }
 }
 
+void doubleLoop(int a[], const int N){
+    int sum = 0, max = INT_MIN;
+    for (int i=0; i<N; i++){
+        for (int j=i; j<N; j++){
+            sum += a[j];
+            if (sum>max){
+                max = sum;
+            }
+        }
+        sum = 0;
+    }
+    if (max == INT_MIN){
+        cout << "How is this even possible!" << endl;
+    }
+    else{
+        cout << max << endl;
+    }
+}
+
+
+
 int main(){
-    const int N = 4;
-    int a[N] = {-1, 4, 7, 2};
+    const int N = 5;
+    int a[N] = {1, -4, 3, 2, 1};
     bruteForce(a, N);
+    doubleLoop(a, N);
     return 0;
 }
