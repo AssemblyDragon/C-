3294f2624c3bd34b7caf913af1ed67d3c3cf8bc1
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-04-30 23:11:16 +0530

    Create ClassesFirstLook.cpp

---
+++ ClassesFirstLook.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class student{
+    int regno;
+    string name;
+    public:
+        student(int regno, string name){
+            this->regno = regno;
+            this->name = name;
+        }
+        ~student(){
+            this->display();
+        }
+        void display(){
+            cout << "Registration Number : " << this->regno << endl;
+            cout << "Name : " << this->name << endl;
+        }
+        void set(int regno, string name){
+            this->regno = regno;
+            this->name = name;
+        }
+};
+
+int main(){
+
+    student s1(1, "First Person");
+    return 0;
+}
