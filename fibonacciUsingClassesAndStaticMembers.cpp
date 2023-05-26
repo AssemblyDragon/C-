48ce5f1205832c74dd3441175e5e09b4b520125e
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-05-26 18:27:21 +0530

    Create fibonacciUsingClassesAndStaticMembers.cpp

---
+++ fibonacciUsingClassesAndStaticMembers.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Fibonacci{
+  static int n;
+  public:
+    static void display(){
+      int a = 0;
+      int b = 1;
+      int temp;
+      cout << "Fibonacci Series : ";
+      for (int i=0; i<n; i++){
+        cout << a << " ";
+        temp = b;
+        b = a+b;
+        a = temp;
+      }
+      cout << endl;
+    }
+};
+
+int Fibonacci::n = 10;
+
+int main(){
+  Fibonacci f;
+  f.display();
+  return 0;
+}
