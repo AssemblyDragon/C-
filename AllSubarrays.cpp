38333500b3443ff1a9d1612eb3374a0e7bc33c48
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-06-07 22:24:56 +0530

    Update AllSubarrays.cpp

--- AllSubarrays.cpp
+++ AllSubarrays.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    int n = 4, a[4] = {1, 2, 3, 4};
+    int n = 4, a[4] = {1, 4, 3, 2};
     for (int i=1; i<=n; i++){
         for (int j=0; j<n-i+1; j++){
             for (int k = j; k<j+i; k++){
