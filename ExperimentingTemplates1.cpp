50f36a22e68cdf7828972cff75a24fa96409d77c
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-06-11 23:44:49 +0530

    Create ExperimentingTemplates1.cpp

---
+++ ExperimentingTemplates1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+template <class T>
+
+T Greater(T a, T b){
+    return (a>=b?a:b);
+}
+
+int main(){
+
+    int n1 = 5, n2 = 6;
+    float f1 = 2.5, f2 = 1.2;
+    cout << Greater(n1, n2) << endl;
+    cout << Greater<float>(f1, f2) << endl;
+
+    return 0;
+}
