62e631e2b2cd6e94288f033e4ae17644c7cd980e
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-06-11 23:45:17 +0530

    Create ExperimentingTemplates2.cpp

---
+++ ExperimentingTemplates2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+template <class U, int N>
+
+U mod(U X){
+    return (X*N);
+}
+
+int main(){
+
+    cout << mod<int, 3>(2) << endl;
+    cout << (float)(mod<float, 2>(2)) << endl;
+
+    return 0;
+}
